fec45210346d8d1d9bb95852a35691e9cf4ec97b
Test User <test@test.com>
2026-07-10 01:45:43 +0000

    gascity 1.3.4

--- Formula/gascity.rb
+++ Formula/gascity.rb
@@ -4,26 +4,26 @@
 class Gascity < Formula
   desc "Orchestration-builder SDK for multi-agent coding workflows"
   homepage "https://github.com/gastownhall/gascity"
-  version "1.3.3"
+  version "1.3.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gastownhall/gascity/releases/download/v1.3.3/gascity_1.3.3_darwin_arm64.tar.gz"
-      sha256 "fc9d694182540232c439b0d79dd7cefe577a8f006efc4f58755235e04713a271"
+      url "https://github.com/gastownhall/gascity/releases/download/v1.3.4/gascity_1.3.4_darwin_arm64.tar.gz"
+      sha256 "3b9c668c60ad9fa525c6df631d31efead9ee412dc3af2754b4eafe83aff8006a"
     else
-      url "https://github.com/gastownhall/gascity/releases/download/v1.3.3/gascity_1.3.3_darwin_amd64.tar.gz"
-      sha256 "b84dc221db28b54fe99797a974ea68e4348542c43710f3997aecb51b3eceb5e1"
+      url "https://github.com/gastownhall/gascity/releases/download/v1.3.4/gascity_1.3.4_darwin_amd64.tar.gz"
+      sha256 "c344bacd0a9d40b66ae7362ca199d976e91841a74519b18ea4fb90b93c246aae"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/gastownhall/gascity/releases/download/v1.3.3/gascity_1.3.3_linux_arm64.tar.gz"
-      sha256 "3708cabda6a2566907a4e5b924b213b957cf96b6b8f18c0e35ad4db8da7d0543"
+      url "https://github.com/gastownhall/gascity/releases/download/v1.3.4/gascity_1.3.4_linux_arm64.tar.gz"
+      sha256 "1a8a45d02240ab7ffec970892491a7ec92aadc86be7b598d2ce95a53320eb1af"
     else
-      url "https://github.com/gastownhall/gascity/releases/download/v1.3.3/gascity_1.3.3_linux_amd64.tar.gz"
-      sha256 "0922a6d3a57877119d9d42ed95e18bfb009f2450f60fd2a6994918d4004cce32"
+      url "https://github.com/gastownhall/gascity/releases/download/v1.3.4/gascity_1.3.4_linux_amd64.tar.gz"
+      sha256 "36cf122a434ac1efc42b88b27a9720a97e2178603b6bb2ca25a040c20b25965c"
     end
   end
 
